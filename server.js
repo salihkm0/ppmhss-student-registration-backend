289339e6443ac6b8ea58c7f1de7fd310d59b4dfb
Muhammed Salih Km <salihkm000@gmail.com>
2026-02-01 09:41:50 +0530

    forntend api updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://ppmhss-student-registration.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
