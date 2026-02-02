ed31a9f70b1de82d631ef5d4e850471f9b4d65cc
Muhammed Salih Km <salihkm000@gmail.com>
2026-02-02 10:15:26 +0530

    api updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://ppmhss-student-registration.vercel.app'],
+    origin: ['http://localhost:5173', 'https://ppmhss-student-registration.vercel.app',"https://nmea.oxiumev.com", "http://nmea.ppmhsskottukkara.com"],
     credentials: true
 }));
 app.use(express.json());
